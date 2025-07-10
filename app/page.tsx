aa511673e3cfe269adde77f1add8f7bf81aa52b0
Mari <marianavca002@outlook.com>
2025-07-09 22:57:40 -0300

    updating

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,77 @@
-import Image from "next/image";
+"use client";
+
+import React, { useEffect, useState } from "react";
+import "./style.css";
+
+function HighlightWord({ text, word, className }: any) {
+  const parts = text.split(new RegExp(`(${word})`, "gi"));
+
+  return (
+    <>
+      {parts.map((part: any, i: number) =>
+        part.toLowerCase() === word.toLowerCase() ? (
+          <span key={i} className={className}>
+            {part}
+          </span>
+        ) : (
+          <span key={i}>{part}</span>
+        )
+      )}
+    </>
+  );
+}
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const fullText =
+    "Olá, meu lindo cacto 🌵. Fiz essa surpresa pra ti, pois faz tempo que queria te entregar algo bonito. Queria te passar a mensagem do quão especial você é para mim e o quanto sou feliz por ter você comigo. Queria te dizer que, acima de tudo, te agradeço por estar ao meu lado e por me ensinar, aos pouquinhos, o que é o amor. Contigo, tenho reaprendido a amar e aprendido como o amor pode ser bonito, expressivo, paciente, fiel e, acima de tudo, que, contanto que tenhamos carinho e disposição para melhorarmos uma pela outra, podemos construir uma relação cada vez mais linda! Obrigada por estar comigo, EU TE AMO.";
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const [displayedText, setDisplayedText] = useState("");
+
+  useEffect(() => {
+    let currentIndex = 0;
+    const interval = setInterval(() => {
+      setDisplayedText(fullText.slice(0, currentIndex + 1));
+      currentIndex++;
+      if (currentIndex === fullText.length) clearInterval(interval);
+    }, 80); // tempo entre letras
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="w-full h-full flex justify-center items-center min-h-screen">
+      <div className="relative">
+        <div className="george ml-50">
+          <div className="shadow"></div>
+          <div className="george_flower"></div>
+          <div className="george_head">
+            <div className="line"></div>
+            <div className="cheek"></div>
+            <div className="eye"></div>
+            <div className="eye"></div>
+          </div>
+          <div className="pot_top"></div>
+          <div className="pot_body"></div>
+          <div className="pot_plate"></div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+        <div
+          className="card absolute p-10 bg-white bg-opacity-90 rounded shadow-lg w-120 border border-l-green-600 border-r-green-600 border-t-green-300 border-b-green-300"
+          style={{
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            zIndex: 10,
+          }}
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          <p className="text-slate-600">
+            <HighlightWord
+              text={displayedText}
+              word="cacto"
+              className="text-green-600"
+            />
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
